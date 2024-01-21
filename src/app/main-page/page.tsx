5620572d9495dd5612597a3018afc88ebc21508d
wallace-lw <wallacelw22@gmail.com>
2024-01-21 18:49:14 -0300

    refactor: apagando classe repetida

--- src/app/main-page/page.tsx
+++ src/app/main-page/page.tsx
@@ -7,7 +7,7 @@ export default function Main() {
   return (
     <MaxWidthWrapper className="min-h-screen pt-32">
       <BillMenuAction />
-      <div className="xs:grid-cols-2 xmd:grid-cols-3 grid w-full grid-cols-1 grid-cols-1 gap-8  py-28 pt-14  md:grid-cols-2  xl:grid-cols-3">
+      <div className="xs:grid-cols-2 xmd:grid-cols-3 grid w-full grid-cols-1 gap-8  py-28 pt-14  md:grid-cols-2  xl:grid-cols-3">
         <BillCard isPaid />
         <BillCard isPaid />
         <BillCard isPaid />
