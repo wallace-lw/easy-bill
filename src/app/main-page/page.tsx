8b33710ce027c4046c3f50c65e17284880ba6bf7
wallace-lw <wallacelw22@gmail.com>
2024-02-20 13:46:37 -0300

    feat: adding responsivity to the main page

--- src/app/main-page/page.tsx
+++ src/app/main-page/page.tsx
@@ -1,19 +1,18 @@
 import BillCard from '@/components/BillCard'
 import BillMenuAction from '@/components/BillMenuAction'
-import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Footer from '../../components/Footer'
-
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 export default function Main() {
   return (
     <MaxWidthWrapper className="min-h-screen pt-32">
       <BillMenuAction />
-      <div className="xs:grid-cols-2 xmd:grid-cols-3 grid w-full grid-cols-1 gap-8  py-28 pt-14  md:grid-cols-2  xl:grid-cols-3">
-        <BillCard isPaid />
-        <BillCard isPaid />
-        <BillCard isPaid />
-        <BillCard isPaid />
-        <BillCard isPaid />
-        <BillCard isPaid />
+      <div className="grid w-full grid-cols-1 gap-8 py-28 pt-14  xs:grid-cols-2 md:grid-cols-2  xmd:grid-cols-3  xl:grid-cols-3">
+        <BillCard />
+        <BillCard />
+        <BillCard />
+        <BillCard />
+        <BillCard />
+        <BillCard />
         <BillCard />
         <BillCard />
         <BillCard />
